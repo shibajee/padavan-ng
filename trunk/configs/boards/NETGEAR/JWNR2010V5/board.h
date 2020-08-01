0a0d602ac95d2366eefcb2bb9024558ff30d7d45
BAKEMONO <32440182+shibajee@users.noreply.github.com>
2020-08-01 13:12:40 +0600

    firmware: Add support Netgear JWNR2010v5

---
+++ trunk/configs/boards/NETGEAR/JWNR2010V5/board.h
@@ -0,0 +1,28 @@
+/* Netgear JWNR2010v5 (N300) */
+
+#define BOARD_PID		"JWNR2010V5"
+#define BOARD_NAME		"JWNR2010V5"
+#define BOARD_DESC		"Netgear JWNR2010v5 Wireless Router"
+#define BOARD_VENDOR_NAME	"Netgear International Inc."
+#define BOARD_VENDOR_URL	"https://www.netgear.com/"
+#define BOARD_MODEL_URL		"https://www.netgear.com/support/product/JWNR2010v5.aspx"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	90
+#define BOARD_GPIO_BTN_RESET	1
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI	72
+#define BOARD_GPIO_LED_POWER	21
+#define BOARD_GPIO_LED_LAN	43
+#define BOARD_GPIO_LED_WAN	19
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
